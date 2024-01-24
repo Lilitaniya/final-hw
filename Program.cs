0a5c52fb98c18c33ebd16416fd0f59abb71ccc1a
Bazhena Filippovskaia <kagome-fortuna@ya.ru>
2024-01-24 14:21:02 +0300

    решена проблема

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] sourceArray = { "Hello", "2", "world", ":-)" };
+string[] newArray = FormingNewArray(size:DeterminingSizeNewArray(sourceArray), array:sourceArray);
+PrintArray(array:sourceArray);
+PrintArray(array:newArray);
+
+
+int DeterminingSizeNewArray(string[] array)
+{
+    int length = 0;
+    for (int i = 0; i < array.Length; i++) if (array[i].Length <= 3) length++;
+
+    return length;
+}
+
+string[] FormingNewArray(int size, string[] array)
+{
+    int position = 0;
+    string[] threeArr = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            threeArr[position] = array[i];
+            position++;
+        }
+    }
+
+    return threeArr;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    Console.Write(string.Join(", ", array));
+    Console.Write("]");
+    Console.WriteLine();
+}
+
